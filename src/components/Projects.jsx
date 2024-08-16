390c9805604f76b728fc437c9f2b820d449efc80
Abhi-hertz <93651229+AE-Hertz@users.noreply.github.com>
2024-08-16 21:56:23 +0530

    switched to hooks

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,153 +1,92 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { PROJECTS } from "../constants";
 import { MdArrowOutward } from "react-icons/md";
 import { motion } from "framer-motion";
 
 function Projects() {
-    let boxVarient = {};
-    let textVariant = {};
+    const [isWidth, setIsWidth] = useState(window.innerWidth <= 1020);
 
-    const isMobile = window.innerWidth <= 1020;
-    console.log(isMobile);
+    useEffect(() => {
+        const handleResize = () => setIsWidth(window.innerWidth <= 1020);
+        window.addEventListener("resize", handleResize);
+        return () => window.removeEventListener("resize", handleResize);
+    }, []);
 
-    if (isMobile) {
-        boxVarient = {
-            hover: {
-                scale: 1.1,
-            },
-            initial: {
-                opacity: 1,
-                y: -20,
-            },
-            inView: {
-                opacity: 1,
-                y: 0,
-            },
-        };
-
-        textVariant = {
-            initial: {
-                opacity: 0,
-                y: 20,
-                scale: 0.8,
-            },
-            inView: {
-                opacity: 0.8,
-                y: 0,
-                scale: 1,
-                transition: {
-                    duration: 0.8,
-                    ease: "easeOut",
-                },
-            },
-        };
-    } else {
-        boxVarient = {
-            hover: {
-                scale: 1.1,
-            },
-            initial: {
-                opacity: 0,
-                y: -20,
-            },
-            inView: {
-                opacity: 1,
-                y: 0,
-            },
-        };
-    }
+    const variants = {
+        visible: { opacity: 1 },
+        hidden: { opacity: 0 },
+    };
 
     return (
-        <>
-            <section className="pt-20" id="projects">
-                <motion.h2
-                    initial={{ opacity: 0, y: -20 }}
-                    whileInView={{ opacity: 1, y: 0 }}
-                    transition={{ duration: 0.8 }}
-                    className="mb-8 text-center text-3xl lg:text-4xl"
-                >
-                    Projects
-                </motion.h2>
-                <div className="grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-3">
-                    {PROJECTS.map((project) => (
+        <section className="pt-20" id="projects">
+            <motion.h2
+                initial={{ opacity: 0, y: -20 }}
+                whileInView={{ opacity: 1, y: 0 }}
+                transition={{ duration: 0.8 }}
+                className="mb-8 text-center text-3xl lg:text-4xl"
+            >
+                Projects
+            </motion.h2>
+            <div className="grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-3">
+                {PROJECTS.map((project) => (
+                    <motion.div
+                        initial={{ opacity: 0, scale: 0.9 }}
+                        whileInView={{ opacity: 1, scale: 1 }}
+                        transition={{ duration: 0.5 }}
+                        whileHover={{ scale: 1.05 }}
+                        key={project.id}
+                        className="group relative overflow-hidden rounded-3xl"
+                    >
+                        <motion.img
+                            whileHover={{ scale: 1.1 }}
+                            src={project.image}
+                            alt={project.name}
+                            className="h-full w-full object-cover transition-transform duration-500 group-hover:scale-110"
+                        ></motion.img>
                         <motion.div
-                            variants={boxVarient}
-                            initial="initial"
-                            whileInView="inView"
+                            initial="hidden"
+                            animate={isWidth ? "visible" : "hidden"}
+                            whileHover="visible"
+                            variants={variants}
                             transition={{ duration: 0.5 }}
-                            whileHover="hover"
-                            key={project.id}
-                            className="group relative overflow-hidden rounded-3xl"
+                            className={`absolute inset-0 flex flex-col items-center justify-center text-white backdrop-blur-lg transition-opacity duration-500 ${
+                                isWidth
+                                    ? "opacity-100"
+                                    : "group-hover:opacity-100"
+                            }`}
                         >
-                            <motion.img
-                                variants={boxVarient}
-                                whileHover="hover"
-                                src={project.image}
-                                alt={project.name}
-                                className="h-full w-full object-cover transition-transform duration-500 group-hover:scale-110"
-                            ></motion.img>
-                            <motion.div
-                                variants={boxVarient}
-                                initial="initial"
-                                whileHover="hover"
-                                transition={{ duration: 0.5 }}
-                                className="absolute inset-0 flex flex-col items-center justify-center text-white opacity-0 backdrop-blur-lg transition-opacity duration-500 group-hover:opacity-100"
-                            >
-                                <motion.h3
-                                    variants={textVariant}
-                                    initial="initial"
-                                    whileInView="inView"
-                                    transition={{ duration: 0.6 }}
-                                    className="mb-2 text-xl"
-                                >
-                                    {project.name}
-                                </motion.h3>
-                                <motion.p
-                                    variants={textVariant}
-                                    initial="initial"
-                                    whileInView="inView"
-                                    transition={{ duration: 0.7 }}
-                                    className="mb-12 p-4"
-                                >
-                                    {project.description}
-                                </motion.p>
+                            <h3 className="mb-2 text-xl">{project.name}</h3>
+                            <p className="mb-12 p-4">{project.description}</p>
 
-                                <motion.div
-                                    variants={textVariant}
-                                    initial="initial"
-                                    whileInView="inView"
-                                    transition={{ duration: 0.8 }}
-                                    className="flex space-x-2"
+                            <div className="flex space-x-2">
+                                <a
+                                    href={project.githubLink}
+                                    target="_blank"
+                                    rel="noopener noreferrer"
+                                    className="rounded-full bg-white px-4 py-4 text-black hover:bg-gray-300 cursor-pointer"
                                 >
-                                    <a
-                                        href={project.githubLink}
-                                        target="_blank"
-                                        rel="noopener noreferrer"
-                                        className="rounded-full bg-white px-4 py-4 text-black hover:bg-gray-300 cursor-pointer"
-                                    >
-                                        <div className="flex items-center cursor-pointer">
-                                            <span>View on Github</span>
-                                            <MdArrowOutward />
-                                        </div>
-                                    </a>
-                                    <a
-                                        href={project.tryLink}
-                                        target="_blank"
-                                        rel="noopener noreferrer"
-                                        className="rounded-full bg-stone-900 px-4 py-4 text-white hover:bg-stone-800 cursor-pointer"
-                                    >
-                                        <div className="flex items-center cursor-pointer">
-                                            <span>Try</span>
-                                            <MdArrowOutward />
-                                        </div>
-                                    </a>
-                                </motion.div>
-                            </motion.div>
+                                    <div className="flex items-center cursor-pointer">
+                                        <span>View on Github</span>
+                                        <MdArrowOutward />
+                                    </div>
+                                </a>
+                                <a
+                                    href={project.tryLink}
+                                    target="_blank"
+                                    rel="noopener noreferrer"
+                                    className="rounded-full bg-stone-900 px-4 py-4 text-white hover:bg-stone-800 cursor-pointer"
+                                >
+                                    <div className="flex items-center cursor-pointer">
+                                        <span>Try</span>
+                                        <MdArrowOutward />
+                                    </div>
+                                </a>
+                            </div>
                         </motion.div>
-                    ))}
-                </div>
-            </section>
-        </>
+                    </motion.div>
+                ))}
+            </div>
+        </section>
     );
 }
 
