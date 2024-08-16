da37d1f07a223669b457d250f60fb20554a1cebd
Abhi-hertz <93651229+AE-Hertz@users.noreply.github.com>
2024-08-16 21:25:51 +0530

    added gtag and project view

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -4,6 +4,58 @@ import { MdArrowOutward } from "react-icons/md";
 import { motion } from "framer-motion";
 
 function Projects() {
+    let boxVarient = {};
+    let textVariant = {};
+
+    const isMobile = window.innerWidth <= 1020;
+    console.log(isMobile);
+
+    if (isMobile) {
+        boxVarient = {
+            hover: {
+                scale: 1.1,
+            },
+            initial: {
+                opacity: 1,
+                y: -20,
+            },
+            inView: {
+                opacity: 1,
+                y: 0,
+            },
+        };
+
+        textVariant = {
+            initial: {
+                opacity: 0,
+                y: 20,
+                scale: 0.8,
+            },
+            inView: {
+                opacity: 0.8,
+                y: 0,
+                scale: 1,
+                transition: {
+                    duration: 0.8,
+                    ease: "easeOut",
+                },
+            },
+        };
+    } else {
+        boxVarient = {
+            hover: {
+                scale: 1.1,
+            },
+            initial: {
+                opacity: 0,
+                y: -20,
+            },
+            inView: {
+                opacity: 1,
+                y: 0,
+            },
+        };
+    }
 
     return (
         <>
@@ -19,31 +71,54 @@ function Projects() {
                 <div className="grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-3">
                     {PROJECTS.map((project) => (
                         <motion.div
-                            initial={{ opacity: 0, scale: 0.9 }}
-                            whileInView={{ opacity: 1, scale: 1 }}
+                            variants={boxVarient}
+                            initial="initial"
+                            whileInView="inView"
                             transition={{ duration: 0.5 }}
-                            whileHover={{ scale: 1.05 }}
+                            whileHover="hover"
                             key={project.id}
                             className="group relative overflow-hidden rounded-3xl"
                         >
                             <motion.img
-                                whileHover={{ scale: 1.1 }}
+                                variants={boxVarient}
+                                whileHover="hover"
                                 src={project.image}
                                 alt={project.name}
                                 className="h-full w-full object-cover transition-transform duration-500 group-hover:scale-110"
                             ></motion.img>
                             <motion.div
-                                initial={{ opacity: 0 }}
-                                whileHover={{ opacity: 1 }}
+                                variants={boxVarient}
+                                initial="initial"
+                                whileHover="hover"
                                 transition={{ duration: 0.5 }}
-                                className="absolute inset-0 flex flex-col items-center  justify-center text-white opacity-0 backdrop-blur-lg transition-opacity duration-500 group-hover:opacity-100"
+                                className="absolute inset-0 flex flex-col items-center justify-center text-white opacity-0 backdrop-blur-lg transition-opacity duration-500 group-hover:opacity-100"
                             >
-                                <h3 className="mb-2 text-xl">{project.name}</h3>
-                                <p className="mb-12 p-4">
+                                <motion.h3
+                                    variants={textVariant}
+                                    initial="initial"
+                                    whileInView="inView"
+                                    transition={{ duration: 0.6 }}
+                                    className="mb-2 text-xl"
+                                >
+                                    {project.name}
+                                </motion.h3>
+                                <motion.p
+                                    variants={textVariant}
+                                    initial="initial"
+                                    whileInView="inView"
+                                    transition={{ duration: 0.7 }}
+                                    className="mb-12 p-4"
+                                >
                                     {project.description}
-                                </p>
+                                </motion.p>
 
-                                <div className="flex space-x-2">
+                                <motion.div
+                                    variants={textVariant}
+                                    initial="initial"
+                                    whileInView="inView"
+                                    transition={{ duration: 0.8 }}
+                                    className="flex space-x-2"
+                                >
                                     <a
                                         href={project.githubLink}
                                         target="_blank"
@@ -66,7 +141,7 @@ function Projects() {
                                             <MdArrowOutward />
                                         </div>
                                     </a>
-                                </div>
+                                </motion.div>
                             </motion.div>
                         </motion.div>
                     ))}
